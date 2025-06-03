25f0c8d700d8b3e2541712312cb4399bcb0a78ea
abioduolayinka <abiodunolayinka75@gmail.com>
2025-06-03 17:54:31 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,20 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: './',
+  base: "./",
+  build: {
+    outDir: "dist", // Ensure output directory matches Vercel's settings
+  },
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
- 
 }));
